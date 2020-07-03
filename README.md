57792dfbe93e649cac460f148c3bbae76410d3f1
Marc Solé <msolefonte@gmail.com>
2020-07-03 16:07:23 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# tww2-vco2-framework
+# Victory Conditions Overhaul 2 Framework for TW:W2
+
+Victory Conditions Overhaul 2 Framework for TW:W2
---
+++ src/script/vco/lib/vco.lua
@@ -0,0 +1,26 @@
+local vco = {};
+
+function vco:get_faction_keys()
+    return ipairs(cm:get_human_factions());
+end
+
+function vco:trigger_faction_missions(mod_name, faction_key)
+    local campaign_name = cm:get_campaign_name();
+    local missions = require("script/"..mod_name.."/factions/"..faction_key.."/campaigns/"..campaign_name.."/missions");
+
+    for _, mission in ipairs(missions) do
+        if mission and mission ~= "" then
+            cm:trigger_custom_mission_from_string(faction_key, mission);
+        end
+    end
+end
+
+function vco:trigger_custom_missions(mod_name)
+    if cm:is_new_game() then
+        for _, faction_key in vco:get_faction_keys() do
+            vco:trigger_faction_missions(mod_name, faction_key);
+        end
+    end
+end
+
+return vco;
